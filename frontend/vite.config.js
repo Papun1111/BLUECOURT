19ba22ef5d63e57d30f4bb7fa89354d9dda09cd5
Papun1111 <gohanmohapatra@gmail.com>
2025-06-16 12:42:42 +0530

    config file add

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,11 +10,5 @@ export default defineConfig({
   ],
   server:{
     port: 3000,
-		proxy: {
-			"/api": {
-				target: "https://bluecourtbackend.onrender.com",
-				changeOrigin: true,
-			},
-    }
   }
 });
